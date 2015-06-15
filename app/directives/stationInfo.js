d4bcb06494ee4f4bd12cc06558491c8ee373da6f
Berend Weij <info@berendweij.nl>
2015-06-15 14:49:30 +0200

    cleanup

--- app/directives/stationInfo.js
+++
@@ -1,8 +0,0 @@
-angular
-    .module('TrainStationDirectives')
-    .directive('stationInfo', function() {
-        return {
-            restrict: 'E',
-            templateUrl: 'app/directives/stationInfo.html'
-        }
-    });
